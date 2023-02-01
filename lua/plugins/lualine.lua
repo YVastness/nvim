a25a8f47eaccfae49786d52f7ff559b32b0d9806
vastness <1916286435@qq.com>
2023-02-01 16:01:58 +0800

    update lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,20 @@
+local function diff_source()
+    local gitsigns = vim.b.gitsigns_status_dict
+    if gitsigns then
+        return {
+            added = gitsigns.added,
+            modified = gitsigns.changed,
+            removed = gitsigns.removed
+        }
+    end
+end
+
 require('lualine').setup({
     options = {
         theme = 'tokyonight'
     },
-    extensions = {'nvim-tree', 'toggleterm'}
+    extensions = {'nvim-tree', 'toggleterm'},
+    sections = {
+        lualine_b = {{'b:gitsigns_head', icon = ''},  {'diff', source = diff_source}, },
+    }
 })
